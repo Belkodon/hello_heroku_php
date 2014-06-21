7a9bc9692eb738e1ece4d61f71721b6707c50e04
Belkodon <joachim.stevens@gmail.com>
2014-06-21 16:51:24 +0200

    use two different index...

---
+++ index_s.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<script type="text/javascript">
+		<!--
+		   window.location="http://xn--gnstig-3ya.stapelstuhl.de";
+		//-->
+	</script>
+
+</head>
+<body>
+
+<?php
+	header( 'Location: http://xn--gnstig-3ya.stapelstuhl.de' ); 
+?>
+
+</body>
+</html> 
