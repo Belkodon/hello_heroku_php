b2e0bb6ef6cde3fe5d9d318d9a387ed1d26fac80
Belkodon <joachim.stevens@gmail.com>
2014-06-21 15:14:50 +0200

    trivial changes

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<script type="text/javascript">
+		<!--
+		   window.location="http://xn--gnstig-3ya.poketi-pokertische.de";
+		//-->
+	</script>
+
+</head>
+<body>
+
+<?php
+	header( 'Location: http://xn--gnstig-3ya.poketi-pokertische.de' ); 
+?>
+
+</body>
+</html> 
